655dc4a97a351c8deee40b820f55efefa600ca14
Maykon <maykon.kazuhiro@ufms.br>
2025-03-30 23:40:44 -0400

    1119

--- 3º Semester/Estrutura_de_Dados/Beecrowd/exer4.py
+++ 3º Semester/Estrutura_de_Dados/Beecrowd/exer4.py
@@ -1,54 +1,48 @@
-while (True):
-    n, k, m = map(int, input().split())
-    if (n, k, m) == (0, 0, 0):
+while True:
+    N, K, M = map(int, input().split())
+    if (N, K, M) == (0, 0, 0):
         break
 
-    numeros = [i for i in range(1, n+1)]
+    v = [i + 1 for i in range(N)]
+    
+    i = 0
+    j = N - 1
+    
+    cont = 0  # Quantidade de números já removidos
+    resultado = []
 
-    saida = ""
-    i, j = 0, n-1
+    while cont < N:
+        passos = K
+        while passos > 0:
+            if v[i] != 0:
+                passos -= 1
+                if passos == 0:
+                    index1 = i  # Índice do número a ser eliminado pela contagem K
+            i = (i + 1) % N
 
-    while (numeros):
-        i = (i + k - 1) % len(numeros)
-        j = (j - m + 1) % len(numeros)
-        
-        if (len(numeros) > 2):
-            if i != j:
-                if i < j:
-                    saida += "  " + str(numeros[i])
-                    saida += "  " + str(numeros[j]) + ","
-                    numeros.pop(j)
-                    numeros.pop(i)
-                    i -= 1
-                    j -= 2
+        passos = M
+        while passos > 0:
+            if v[j] != 0:
+                passos -= 1
+                if passos == 0:
+                    index2 = j  # Índice do número a ser eliminado pela contagem M
+            j = (j - 1 + N) % N  # Ajusta a posição de forma circular para trás
 
-                else:
-                    saida += "  " + str(numeros[i])
-                    saida += "  " + str(numeros[j]) + ","
-                    numeros.pop(i)
-                    numeros.pop(j)
-                    i -= 2
-                    j -= 1
+        # Se não for a primeira impressão, adiciona vírgula antes do par ou do único número
+        if cont > 0:
+            resultado.append(",")
 
-            else:
-                saida += "  " + str(numeros.pop(i)) + ","
-                i -= 1
-                j -= 1
-
-        elif (len(numeros) == 1):
-            saida += "  " + str(numeros.pop(i))
-
+        # Se os índices selecionados forem diferentes, elimina dois números
+        if index1 != index2:
+            resultado.append(f"{v[index1]:3d}{v[index2]:3d}")
+            cont += 2
         else:
-            if i < j:
-                saida += "  " + str(numeros[i])
-                saida += "  " + str(numeros[j])
-                numeros.pop(j)
-                numeros.pop(i)
+            resultado.append(f"{v[index1]:3d}")
+            cont += 1
 
-            else:
-                saida += "  " + str(numeros[i])
-                saida += "  " + str(numeros[j])
-                numeros.pop(i)
-                numeros.pop(j)
+        # Marca os números eliminados com zero
+        v[index1] = 0
+        v[index2] = 0
 
-    print(saida)
+    # Exibe a saída formatada conforme o esperado
+    print("".join(resultado))
