b64103de7fccddb76330a86468c0fb4664711f38
Maykon <tamanahakazuhiro@gmail.com>
2025-05-10 18:48:29 -0400

    Lista do Beecrowd

---
+++ 3º_Semester/Estrutura_de_Dados/Lista_do_beecrowd/Lista_2/Beecrowd_1498.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, x, i, j;
+
+    while (cin >> n) {
+        vector<long long> frequencias[64];
+
+        for (i = 0; i < n; i++) {
+            cin >> x;
+            frequencias[x].push_back(-1);
+        }
+
+        long long total_valor = 0;
+        long long atual_valor = 1;
+
+        for (i = 55; i >= 1; i--) {
+            for (j = 0; j < frequencias[i].size(); j++) {
+                if (frequencias[i][j] == -1) {
+                    frequencias[i][j] = atual_valor;
+                    total_valor += atual_valor;
+                }
+            }
+
+            for (j = 0; j < frequencias[i].size(); j += 2) {
+                if (j + 1 < frequencias[i].size()) {
+                    if (i > 1) {
+                        frequencias[i-1].push_back(frequencias[i][j] +frequencias[i][j+1]);
+                    }
+
+                    atual_valor = max(atual_valor, frequencias[i][j]);
+                    atual_valor = max(atual_valor, frequencias[i][j+1]);
+                }
+            }
+        }
+
+        cout << total_valor << '\n';
+    }
+
+    return 0;
+}
