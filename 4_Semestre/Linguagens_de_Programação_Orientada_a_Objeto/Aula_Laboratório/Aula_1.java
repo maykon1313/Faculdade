6bcea9593dc227fedbdc62f7efb397fe43c5e2bc
maykon <tamanahakazuhiro@gmail.com>
2025-08-13 15:42:37 -0400

    4_Semestre

---
+++ 4_Semestre/Linguagens_de_Programação_Orientada_a_Objeto/Aula_Laboratório/Aula_1.java
@@ -0,0 +1,101 @@
+package Aula_Laboratório;
+
+class Dog {
+    int peso;
+    String raca;
+    String nome;
+
+    public void latir() {
+        System.out.println(nome + " está latindo.");
+    }
+}
+
+class Movie {
+    String titulo;
+    String genero;
+    float classificacao;
+    
+    public void play() {
+        System.out.println("O filme de nome " + titulo + " do gênero " + genero + " que possui a classificação de " + classificacao + " estrelas está sendo exibido.");
+    }
+}
+
+class GuessGame {
+    Player p1;
+    Player p2;
+    Player p3;
+
+    public void startGame() {
+        p1 = new Player();
+        p2 = new Player();
+        p3 = new Player();
+
+        int targetNumber = (int)(Math.random() * 10);
+
+        while (true) {
+            System.out.println("O número é " + targetNumber + ".");
+            
+            p1.guess();
+            p2.guess();
+            p3.guess();
+
+            if (p1.number == targetNumber) {
+                System.out.println("Player 1 acertou o número, era: " + p1.number + ".");
+                break;
+            } else {
+                System.out.println("Player 1 errou e chuto o número: " + p1.number + ".");
+            }
+            
+            if (p2.number == targetNumber) {
+                System.out.println("Player 2 acertou o número, era: " + p2.number + ".");
+                break;
+            } else {
+                System.out.println("Player 2 errou e chuto o número: " + p2.number + ".");
+            }
+            
+            if (p3.number == targetNumber) {
+                System.out.println("Player 3 acertou o número, era: " + p3.number + ".");
+                break;
+            } else {
+                System.out.println("Player 3 errou e chuto o número: " + p3.number + ".");
+            }
+
+            System.out.println();
+        }
+    }
+}
+
+class Player {
+    int number;
+
+    public void guess() {
+        number = (int)(Math.random() * 10);
+    }
+}
+
+public class Aula_1 {
+    public static void main(String[] args) {
+        /* 
+        Dog cachorro = new Dog();
+
+        cachorro.nome = "Jorgete";
+        cachorro.peso = 50;
+        cachorro.raca = "Humana";
+
+        cachorro.latir();
+        */
+    
+        /*
+        Movie filme = new Movie();
+
+        filme.titulo = "Wall-E";
+        filme.genero = "Animação";
+        filme.classificacao = 8.4f;
+    
+        filme.play();
+        */
+
+        GuessGame jogo = new GuessGame();
+        jogo.startGame();
+    }    
+}
---
+++ 4_Semestre/Linguagens_de_Programação_Orientada_a_Objeto/Estudo/Main.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello world!");
+
+        not_main();
+    }
+
+    public static void not_main() {
+        System.out.println("Oh my gooooosh!!!");
+    }
+ 
+}
